54b52ae4c71fd8cd72d26857c58901d59e4018b0
23fajar23 <fajardarussalam20@gmail.com>
2024-05-15 04:14:09 +0700

    Add project files.

---
+++ Simple_Logic_Program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Logic_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
